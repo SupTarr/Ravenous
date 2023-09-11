7e28f829e9c703cfcf79d0b2dd2123dd107d8d97
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-09-11 22:21:17 +0700

    feat: remove mockup data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,8 @@
 import { useState } from "react";
 import "./App.css";
-import Business from "../Business/Business.jsx";
 import BusinessList from "../BusinessList/BusinessList.jsx";
 import SearchBar from "../SearchBar/SearchBar.jsx";
 import Yelp from "../../util/Yelp";
-
-const business = {
-  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Flavortown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
-  rating: 4.5,
-  reviewCount: 90,
-};
-const businesses = [business, business, business, business, business, business];
 
 function App(props) {
   const [businesses, setBusinesses] = useState([]);
